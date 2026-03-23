0963bdba0f42b106c83bab5bfa74fff6ada9b1dc
vinkip <vinkiplagat02@gmail.com>
2026-03-23 10:22:56 +0300

    Final Commit

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+function calculateTax(amount) {
+    return amount * 0.10;
+
+}
+
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
+
+function findMaximum(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+}
+
+function isPalindrome(word) {
+    const reversed = word.split('').reverse().join('');
+    return word === reversed;
+}
 
 
-
-
+function calculateDiscountedPrice(price, discountPercent) {
+    return price * (1 - discountPercent / 100);
+}
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
